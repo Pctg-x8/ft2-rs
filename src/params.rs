63201b19575ba89868198019800a3538675c4356
S.Percentage <Syn.Tri.Naga@gmail.com>
2019-09-07 22:32:16 +0900

    fix: tag type

--- src/params.rs
+++ src/params.rs
@@ -1,10 +1,10 @@
 
 use crate::*;
 
-pub const FT_PARAM_TAG_IGNORE_TYPOGRAPHIC_FAMILY: FT_ULong = FT_MAKE_TAG!('i', 'g', 'p', 'f');
-pub const FT_PARAM_TAG_IGNORE_TYPOGRAPHIC_SUBFAMILY: FT_ULong = FT_MAKE_TAG!('i', 'g', 'p', 's');
-pub const FT_PARAM_TAG_INCREMENTAL: FT_ULong = FT_MAKE_TAG!('i', 'n', 'c', 'r');
-pub const FT_PARAM_TAG_LCD_FILTER_WEIGHTS: FT_ULong = FT_MAKE_TAG!('l', 'c', 'd', 'f');
-pub const FT_PARAM_TAG_RANDOM_SEED: FT_ULong = FT_MAKE_TAG!('s', 'e', 'e', 'd');
-pub const FT_PARAM_TAG_STEM_DARKENING: FT_ULong = FT_MAKE_TAG!('d', 'a', 'r', 'k');
-pub const FT_PARAM_TAG_UNPATENTED_HINTING: FT_Long = FT_MAKE_TAG!('u', 'n', 'p', 'a');
+pub const FT_PARAM_TAG_IGNORE_TYPOGRAPHIC_FAMILY: FT_Tag = FT_MAKE_TAG!('i', 'g', 'p', 'f');
+pub const FT_PARAM_TAG_IGNORE_TYPOGRAPHIC_SUBFAMILY: FT_Tag = FT_MAKE_TAG!('i', 'g', 'p', 's');
+pub const FT_PARAM_TAG_INCREMENTAL: FT_Tag = FT_MAKE_TAG!('i', 'n', 'c', 'r');
+pub const FT_PARAM_TAG_LCD_FILTER_WEIGHTS: FT_Tag = FT_MAKE_TAG!('l', 'c', 'd', 'f');
+pub const FT_PARAM_TAG_RANDOM_SEED: FT_Tag = FT_MAKE_TAG!('s', 'e', 'e', 'd');
+pub const FT_PARAM_TAG_STEM_DARKENING: FT_Tag = FT_MAKE_TAG!('d', 'a', 'r', 'k');
+pub const FT_PARAM_TAG_UNPATENTED_HINTING: FT_Tag = FT_MAKE_TAG!('u', 'n', 'p', 'a');
